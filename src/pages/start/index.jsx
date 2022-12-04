ed192a6f20237c47b60a278614d9f22d1a4458f3
Gabriel <g.tabordamoreira@gmail.com>
2022-12-04 18:52:19 -0400

    Estrutura de Paginas e rotas

---
+++ src/pages/start/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {View, Text, Image, scrollView} from "react-native";
+
+
+export default function Start(){
+
+
+    return(
+        <View>
+            <ScrollView showVerticalScrollIndicator={false}>
+                <View>
+                    <Image
+                        source={require("../../assets/icons/logo3.png")}
+                        />
+                    <Text>
+                        Vamos transformar sua vida {"/n"} em jogo, buscando sempre  {"/n"} o melhor nivel.
+                    </Text>
+                </View>
+
+            </ScrollView>
+        </View>
+    )
+}
